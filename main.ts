f5175a16b3329b7b6abb8d74a99060de76ac9ce1
Hammadabbasi17 <165670673+Hammadabbasi17@users.noreply.github.com>
2024-06-23 13:48:45 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,817 @@
+import inquirer from "inquirer";
+     
+
+
+// // ---------------- Question 2 ------------  // //
+
+
+// let name = "Hammad";
+// console.log(`Hello ${name}, would you like to learn some python today?`);
+
+// // ------------- Question 3--------------------- //
+
+// let name1 = "hammad";
+// console.log(name1.toLowerCase());
+// console.log(name1.toUpperCase());
+// console.log(name1.toString());
+
+
+// //------------------- Question 4 ------------- // //
+
+// let myQuote : string = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”'
+// console.log(myQuote);
+
+
+// // ----------------Question 5 -----------------// //
+
+// let famousPerson = "Albert Einstein";
+
+// let myQuote : string = '"A person who never made a mistake never tried anything new."'
+// let message = ` ${famousPerson} once said, ${myQuote} `;
+
+// console.log(message);
+
+
+// // ----------------Question 6 -----------------// //
+
+// let name = " Hammad ";
+// console.log(name);
+// let name1 = "\tHammad";
+// console.log(name1);
+
+// let name2 = "\nHammad";
+// console.log(name2);
+
+
+
+// // ----------------Question 7 -----------------// //
+
+ 
+ 
+//  console.log(( 5+ 3));
+// console.log((20-12));
+// console.log((4*2));
+// console.log((32/4));
+
+
+// // ----------------Question 8 -----------------// //
+// console.log((4 + 4));
+// console.log((20-12));
+// console.log((4*2));
+// console.log((16/2));
+
+
+
+// // ----------------Question 9 -----------------// //
+
+
+// let favouriteNumber : number = 7;
+// let message : string = `My favourite Number is ${favouriteNumber}`;
+// console.log(message);
+
+
+
+
+// // ----------------Question 10 -----------------// //
+
+// let num1 = 10;
+// let num2 = 20;
+// console.log(`addition ${num1 + num2}`);
+
+
+
+
+// // ----------------Question 11 -----------------// //
+
+// let arr = [ "Sajjad" , "Naemm" , "Ahmed" , "Ali"];
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+// // ----------------Question 12 -----------------// //
+
+
+// let freinds = [ "Sajjad" , "Naemm" , "Ahmed" , "Ali"];
+
+// console.log(`Hello ${freinds[0]} how are you? `);
+// console.log(`Hello ${freinds[1]} how are you? `);
+// console.log(`Hello ${freinds[2]} how are you? `);
+// console.log(`Hello ${freinds[3]} how are you? `);
+
+
+
+// // ----------------Question 13 -----------------// //
+
+
+//  let arr  = [ "car" , "train" , "motorbike" , "helecophter"];
+
+// for(let i = 0; i <= 3; i++){
+
+//     console.log(`i Would like to own a ${ arr[i]}`);
+    
+
+// }
+
+
+
+
+// // ----------------Question 14 -----------------// //
+
+// let Freinds = [ "Usman" , "Waris" , "huzaifa" , "yaqoob"];
+// for(let a = 0; a<Freinds.length ; a++){
+//     console.log(`Mr ${Freinds[a]}  I Would like to invite for dinner`);
+    
+// }
+
+// // ---------------- Question 15 -----------------// //
+
+// let Freinds = [ "Usman" , "Waris" , "huzaifa" , "yaqoob"];
+// console.log(`${Freinds[0] } is not coming for dinner`);
+
+// console.log("i invited to hammad please come for dinner ");
+
+// let replace = Freinds.splice(0 , 1 , "hammad");
+// console.log(replace);
+
+// for(let a = 0 ; a<Freinds.length ; a++ ){
+//     console.log(Freinds[a]);
+    
+    
+// }
+
+
+
+// // // ---------------- Question 16 -----------------// //
+
+
+
+// let Freinds = [ "Usman" , "Waris" , "huzaifa" , "yaqoob"];
+// console.log(`${Freinds[0] } is not coming for dinner`);
+
+// console.log("i invited to hammad please come for dinner ");
+
+// let replace = Freinds.splice(0 , 1 , "hammad");
+// console.log(replace);
+
+// for(let a = 0 ; a<Freinds.length ; a++ ){
+//     console.log(Freinds[a]);
+    
+    
+//  }
+
+// console.log("i have found a bigger table so please i want to invite more people");
+// Freinds.unshift("sajjad");
+//  Freinds.splice(3 , 0 , "imtiaz");
+//  Freinds.push("farhan");
+// console.log(Freinds);
+
+    
+    
+ // // ---------------- Question 17 -----------------// //
+
+
+
+// let Freinds = [ "Usman" , "Waris" , "huzaifa" , "yaqoob"];
+// console.log(`${Freinds[0] } is not coming for dinner`);
+
+// console.log("i invited to hammad please come for dinner ");
+
+// let replace = Freinds.splice(0 , 1 , "hammad");
+// console.log(replace);
+
+// for(let a = 0 ; a<Freinds.length ; a++ ){
+//     console.log(Freinds[a]);
+    
+    
+//  }
+
+// console.log("i have found a bigger table so please i want to invite more people");
+// Freinds.unshift("sajjad");
+//  Freinds.splice(3 , 0 , "imtiaz");
+//  Freinds.push("farhan");
+// console.log(Freinds);
+
+
+
+//     console.log(" i can invite only two people for dinner" );
+    
+// while(Freinds.length> 2){
+//     let a = Freinds.pop();
+//     console.log(`${a} Sorry you can not invite them to dinner`);
+    
+// }
+// console.log(`${Freinds[0]} You are still invited for the dinner \n ${Freinds[1]} You are still invited for the dinner `);
+// ;
+// Freinds.splice(0 , 2);
+// console.log(Freinds);
+
+
+    
+ // // ---------------- Question 18 -----------------// //
+
+// let location = [" Saudia Arabia " , " Malasyia " , " Iran " , " London "];
+
+// let arr = location.slice();
+// let arr1 = location.sort();
+// console.log(arr);
+// console.log(arr1);
+
+
+// let arr3 = location.reverse();
+// console.log(arr3);
+
+
+// console.log(arr);
+
+
+// let arr4 = location.reverse();
+// console.log(arr4);
+// let order = arr4.reverse();
+// console.log(order);
+
+
+// let arr5 = location.sort();
+// console.log(arr5);
+// let arr6 = location.reverse();
+// console.log(arr6);
+
+
+
+
+
+
+ // // ---------------- Question 19 -----------------// //
+
+// console.log(`the number of people  i invited for dinner is ${Freinds.length}`);
+
+
+ // // ---------------- Question 20 -----------------// //
+
+// let cities = ["karachi","islamabad", "Lahore","Faislabad"];
+// for( let i = 0; i<cities.length; i++){
+//     console.log(`${i} ${cities[i]}`);
+    
+// }
+
+ // // ---------------- Question 21 -----------------// //
+// let object = {
+//      name : "hammad",
+//     age :21,
+//      gender:"male"
+// }
+// console.log(object);
+
+ // // ---------------- Question 22 -----------------// //
+
+//  let array = ["hammad", "sajjad", "naeem"];
+//  console.log(array[8]);
+//  console.log(array[0]);
+ 
+
+
+ // // ---------------- Question 23 -----------------// //
+
+//  let car = "honda";
+//  // test 1
+//  console.log('car === "Honda". this statement i predict true.');
+//  console.log(car === "honda");
+//  // test 2
+//  console.log('car.includes("h"). this statement i predict true.');
+//  console.log(car.includes("h"));
+//  //test 3
+//  console.log('car.toLowerCase() === "honda". this statement i predict true.');
+//  console.log(car.toLowerCase() === "honda");
+//  // test 4
+
+//  console.log('car.length > 0. this statement i predict true.');
+
+//  console.log(car.length >0);
+
+//  // test 5
+//  console.log('car.length === 5. this statement i predict true.')
+// console.log(car.length === 5);
+
+// //false statement
+
+// console.log('car === "Alto". this statement i predict False.');
+//  console.log(car === "Alto");
+//  // test 2
+//  console.log('car.includes("f"). this statement i predict false.');
+//  console.log(car.includes("f"));
+//  //test 3
+//  console.log('car.toUppercase() === "honda". this statement i predict False.');
+//  console.log(car.toUpperCase() === "honda");
+//  // test 4
+
+//  console.log('car.length > 10. this statement i predict False.');
+
+//  console.log(car.length >10);
+
+//  // test 5
+//  console.log('car.length === 6. this statement i predict False.')
+// console.log(car.length === 6);
+
+ 
+
+ // // ---------------- Question 24 -----------------// //
+
+
+//  let  string1 = "faraz";
+//  let string2  =  "Ahmed";
+
+//  console.log(string1 !== string2); // True
+//  console.log(string1 === string2); // False
+ 
+ 
+//  let case1 = "FARAZ";
+//  let case2 = "faraz";
+//  console.log(case1.toLowerCase() === case2);// True
+//  console.log(case1.toLowerCase() !== case2.toLowerCase()); // False
+ 
+// // Numerical test
+//  let num1 = 40;
+//  let num2 = 80;
+//  console.log(num1 < num2);// True
+// console.log(num1 > num2); // False
+
+// console.log(num1 <= num2);// True
+// console.log(num1 >= num2); // False
+
+// // logical test
+
+// let a = 5;
+// let b = 10;
+// console.log(a > 0 && b <20); // True
+// console.log(a > 0 && b < 8); // False
+// console.log( a > 0 || b > 30); // True
+// console.log(a === 0 || b === 20); // False
+
+// let arr = ["city" , "country"];
+// console.log(arr.includes("city")  === true); // True
+// console.log(arr.includes("ali")  === true);  // False
+
+
+
+
+ // // ---------------- Question 25-----------------// //
+
+
+// let alein_colour = "green" ;
+// if(alein_colour === "green"){
+//     console.log("The player just earned 5 points");
+    
+// }
+//  if (alein_colour === "yellow"){
+//     console.log("you are fails");
+    
+// }
+
+// // ---------------- Question 26 -----------------// //
+ 
+// let alein_colour2 = "green" ;
+// if(alein_colour2 === "green"){
+//     console.log("The player just earned 5 points");
+    
+// }else if(alein_colour2 === "yellow"){
+//     console.log("the player just earned 10 points.");
+    
+// }
+
+ 
+// let alein_colour2 = "yellow" ;
+// if(alein_colour2 === "green"){
+//     console.log("The player just earned 5 points");
+    
+// }else if(alein_colour2 === "yellow"){
+//     console.log("the player just earned 10 points.");
+    
+// }
+
+
+
+// // ---------------- Question 27 -----------------// //
+
+
+// let alein_colour3 = "green";
+// if(alein_colour3 === "green"){
+//     console.log("the player earned 5 Poibts");
+    
+// }else if(alein_colour3 === "yellow"){
+//     console.log("the player earned 10 Points");
+    
+// }else if(alein_colour3 === "red"){
+//     console.log("the player earned 15 Points");
+
+// }
+
+
+
+
+// let alein_colour3 = "yellow";
+// if(alein_colour3 === "green"){
+//     console.log("the player earned 5 Poibts");
+    
+// }else if(alein_colour3 === "yellow"){
+//     console.log("the player earned 10 Points");
+    
+// }else if(alein_colour3 === "red"){
+//     console.log("the player earned 15 Points");
+
+// }
+
+
+// let alein_colour3 = "red";
+// if(alein_colour3 === "green"){
+//     console.log("the player earned 5 Poibts");
+    
+// }else if(alein_colour3 === "yellow"){
+//     console.log("the player earned 10 Points");
+    
+// }else if(alein_colour3 === "red"){
+//     console.log("the player earned 15 Points");
+
+// }
+
+
+// // ---------------- Question 28 -----------------// //
+
+
+// let men_age : number = 30;
+// if(men_age < 2){
+//     console.log("the person is a baby.");
+    
+// }else if(men_age >= 2 && men_age < 4){
+//     console.log("the person is a toddler.");
+// }else if (men_age >=4 && men_age < 13 ){
+//     console.log("the person is a Kid.");
+    
+// }else if(men_age >= 13 && men_age < 20){
+//     console.log("the person is a teenager.");
+// }else if(men_age >= 20 && men_age < 65){
+//     console.log("the person is a adult.");
+// }else if (men_age > 65){
+//     console.log("the person is an elder");
+    
+// }
+
+
+// // ---------------- Question 29 -----------------// //
+
+// let favourite_fruit = ["banana","apple","watermelon",];
+// if(favourite_fruit.includes("banana")){
+//     console.log("i realy like banana!");
+    
+// }if(favourite_fruit.includes("watermelon")){
+//     console.log("i realy like watermelon!");
+    
+// }if(favourite_fruit.includes("apple")){
+//     console.log("i realy like apple!");
+    
+// }if(favourite_fruit.includes("mango")){
+//     console.log("i realy like mango!");
+    
+// }if(favourite_fruit.includes("orange")){
+//     console.log("i realy like orange!");
+    
+// }
+
+
+
+// // ---------------- Question 30 -----------------// //
+
+
+
+// let helloadmin = ["Hammad admin","Ali", "Sarfaraz","zeeshan","Bilal"];
+// for(let i =0 ; i<helloadmin.length ; i++){
+//     if(helloadmin.includes("Hammad admin")){
+//         console.log("Hello admin, Would you like to see a status report");
+//        break 
+//     }else {
+//         console.log(`Hellow ${helloadmin[i]} thank you for logging in again`);
+        
+//     }
+//}
+
+
+
+
+
+// let helloadmin = ["sajjad","Ali", "Sarfaraz","zeeshan","Bilal"];
+// for(let i =0 ; i<helloadmin.length ; i++){
+//     if(helloadmin.includes("Hammad")){
+//         console.log("Hello admin, Would you like to see a status report");
+//        break 
+//     }else {
+//         console.log(`Hellow ${helloadmin[i]} thank you for logging in again`);
+        
+//     }
+// }
+
+
+
+// // ---------------- Question 31 -----------------//
+
+
+// let N_user = ["Hammad admin","Ali", "Sarfaraz","zeeshan","Bilal"];
+
+
+
+// if(N_user.length === 0){
+//     console.log("We need to find some users!");
+    
+// }else{
+//     N_user.splice(0 , 5);
+//     console.log("All users has been removed");
+    
+// }
+
+
+// // ---------------- Question 32 -----------------//
+
+
+// let current_users :string[] = ["Naeem", "Ahmed" , "Mursalin", "farhan", "fahad"];
+// let new_user : string[] = ["Naeem", "Ahmed" , "usman", "sajjad", "ali"];
+// new_user.forEach(new_user=>{
+//     let lower1 = new_user.toLowerCase();
+
+//     let c = current_users.some(current_users => current_users.toLowerCase() === lower1)
+
+
+// if(c){
+//     console.log(`is user ${new_user} is not available`);
+    
+// }else{
+//     console.log(`is user ${new_user} is available`);
+    
+// }
+
+// })
+
+
+
+
+
+
+// // ---------------- Question 33 -----------------//
+
+// let num1 : number[] = [1 , 2 , 3 , 4, 5, 6, 7, 8, 9 ];
+// for(let num of num1){ 
+//     if(num === 1){
+//         console.log(`${num}st`);
+        
+//     }else if (num === 2 ){
+//         console.log(`${num}nd`);
+        
+//     }else if (num === 3 ){
+//         console.log(`${num}rd`);
+        
+//     }else{
+//         console.log(`${num}th`);
+        
+//     }
+    
+
+// }
+
+
+
+// // ---------------- Question 34 -----------------// //
+
+// let pizza = ["cheese pizza" , "BBQ chicken pizza", "Meat pizza"];
+// for(let a = 0 ; a<pizza.length; a++){
+//     console.log(` I like ${pizza[a]}`);
+    
+// }
+// console.log("cheese pizza , BBQ pizza , Meat pizza are so yummy.\n its taste is very good.\n i realy love pizza! ");
+
+
+
+// // ---------------- Question 35 -----------------// //
+
+// let animal = [ "cat" , "dog" , "tiger"];
+// for ( let a of animal){
+//     console.log(`A ${a} would make a great pet.`);
+    
+// }
+// console.log("Any of these animal would make a great pet!");
+
+
+// // ---------------- Question 36 -----------------// //
+
+
+//  const make_shirt =(size : string, message : string) => {
+
+// console.log(`The size of T-Shirt is ${size},The message printed on it. ${message} `);
+
+//  }
+// make_shirt("large", "'NEW YORK CITY'");
+ 
+
+
+// // ---------------- Question 37 -----------------// //
+
+// const make_shirt =( message : string ,size : string = " large" ) =>{
+
+//    return size + message;
+   
+//     }
+//   let a = make_shirt(" i love typescript" , );
+// console.log(a);
+
+
+
+// const make_shirt =( message : string ,size : string = " medium" ) =>{
+
+// return message + size;
+//     }
+//   let a = make_shirt(" i love typescript" , );
+// console.log(a);
+
+
+// const make_shirt =( message : string ,size : string  ) =>{
+
+//    return message + size;
+   
+//     }
+//   let a = make_shirt(" i love typescript" , " any size"  );
+// console.log(a);
+
+
+
+
+
+// // ---------------- Question 38 -----------------// //
+
+// const  describe_city=(name : string, country : string = "Pakistan")=>{
+   
+// console.log(`${name} is in ${country}`)
+// }
+
+// describe_city("karachi" , );
+
+
+
+// const  describe_city=(name : string, country : string = "Pakistan")=>{
+   
+//    return `${name} is in ${country}`
+//    }
+   
+//   let a =  describe_city("islamabad" , );
+
+// let b =  describe_city("hyderabad" , );
+// let c =  describe_city("Lahore" ,"pakistan" );
+// console.log(a)
+// console.log(b);
+// console.log(c);
+
+
+
+// // ---------------- Question 39 -----------------// //
+
+
+
+
+// const city_country =(name : string , country : string) =>{
+//    return ` "${name} , ${country}"`;
+// }
+// let a = city_country( "Lahore" ," pakistan");
+// console.log(a);
+
+
+// let b = city_country( "Mumbai" ,"india");
+// console.log(b);
+
+
+
+// let c = city_country( "george town" ,"Malaysia");
+// console.log(c);
+
+
+
+
+// // // ---------------- Question 40 -----------------// //
+// function myalbum(artistname: string, albumtitle : string){
+//     return {artistname , albumtitle};
+// }
+
+// let album1 = myalbum("Beyonce","Lemonade");
+// let album2 = myalbum("Fleetwood mac","Rumours");
+// let album3 = myalbum("Nirvana","Nevermind");
+// console.log(album1);
+// console.log(album2);
+// console.log(album3);
+
+
+
+// function myalbum(artistname: string, albumtitle : string,tracks?: number){
+//     return {artistname , albumtitle,tracks};
+// }
+
+// let album1 = myalbum("Beyonce","Lemonade", 10);
+// let album2 = myalbum("Fleetwood mac","Rumours",40);
+// let album3 = myalbum("Nirvana","Nevermind");
+// console.log(album1);
+// console.log(album2);
+// console.log(album3);
+
+
+// // // ---------------- Question 41 -----------------// //
+
+
+// let a : string[] = ["Hammad" ,"sajjad","ali" ];
+// function mymagic(a : string[]){
+// for(let b of a){
+//     console.log(b);
+    
+// }
+    
+// }
+
+
+// mymagic(a);
+
+
+// // // ---------------- Question 42 -----------------// //
+
+
+
+// let a : string[] = ["Hammad" ,"sajjad","ali" ];
+// function make_great(a : string[]){
+// for(let b of a){
+//     console.log(`The Great to ${b}`);
+    
+// }
+    
+// }
+// make_great(a);
+
+
+// // // ---------------- Question 43 -----------------// //
+
+
+// let a : string[] = ["Hammad" ,"sajjad","ali" ];
+
+
+// let b = [...a];
+// function  make_great(now1 : string){
+//     let show_magician = "";
+// for(let b of a){
+//     show_magician += `${now1} ${b}\n` 
+    
+// }
+//     return show_magician;
+// }
+// // make_great(a);
+
+// let call = make_great("Great");
+// let call1 = call.split(`\n`);
+// console.log(call);
+// console.log(call1);
+// console.log(a);
+
+
+
+// // // ---------------- Question 44 -----------------// //
+
+// function mysandwiches(...items : string[]){
+//     return `i want sandwitch of ${items}`;
+// }
+// let any = mysandwiches("Bacon" ,"Mayo","turkey");
+// let any1 = mysandwiches("chicken sandwich" , "Ham");
+// let any2 = mysandwiches();
+// console.log(any);
+// console.log(any1);
+// console.log(any2);
+
+
+
+
+// // // ---------------- Question 45 -----------------// //
+
+// function car_details ( manufacturer: string, modelname : string, ... addinfo: any[]){
+// const car = {manufacturer, modelname, ...Object.fromEntries(addinfo)};
+// return car
+// }
+
+// const my_car = car_details("Toyota","corolla",["colour","black"],[ "year", "2022"]);
+// console.log(my_car);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
